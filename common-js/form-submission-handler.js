5a07525b2d2d8d448859acbc40fc0f09e06b334c
dleonard <dleonard@goodlifefitness.com>
2019-03-13 21:49:07 -0400

    updated css and finished the loading gif

--- common-js/form-submission-handler.js
+++ common-js/form-submission-handler.js
@@ -128,7 +128,7 @@
         jQuery.fn.center = function () {
             this.css("position", "absolute");
             this.css("top", Math.max(0, (($(window).height() - $(this).outerHeight()) / 2) +
-                $(window).scrollTop()) + "px");
+                $(window).scrollTop()) / 2 + "px");
             this.css("left", Math.max(0, (($(window).width() - $(this).outerWidth()) / 2) +
                 $(window).scrollLeft()) + "px");
             return this;
